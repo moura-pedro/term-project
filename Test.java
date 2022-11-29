e8ba0a329c7e48fea80b62d30351bff38ef5e2ad
Pedro Moura <pedro.moura@eduzz.com>
2022-11-29 13:17:20 -0500

    almost a working trie

--- Test.java
+++ Test.java
@@ -1,13 +1,17 @@
 public class Test {
    public static void main(String[] args) {
       Trie testTrie = new Trie();
-      testTrie.insert("pedro");
-      testTrie.insert("Case");
-      testTrie.insert("xablau");
+      testTrie.root.insert("pedro");
+      testTrie.root.insert("pedra");
+      testTrie.root.insert("pietro");
+      testTrie.root.insert("Case");
+      testTrie.root.insert("xablau");
       
-      System.out.println(testTrie.search("xablau"));
-      System.out.println(testTrie.search("Case"));
-      System.out.println(testTrie.search("jonb"));
+      System.out.println(testTrie.find("xablau"));
+      System.out.println(testTrie.find("Case"));
+      System.out.println(testTrie.find("jonb"));
+
+      System.out.println(testTrie.suggest("pe"));
       
 
 
--- Trie.java
+++ Trie.java
@@ -20,40 +20,46 @@ public class Trie {
       }
 
       public void insert(String word) {
-         if (word == null || word.isEmpty())
-               return;
+         if (word == null || word.isEmpty()) {
+            return;
+         }
          char firstChar = word.charAt(0);
          TrieNode child = children.get(firstChar);
          if (child == null) {
-               child = new TrieNode(firstChar);
-               children.put(firstChar, child);
+            child = new TrieNode(firstChar);
+            children.put(firstChar, child);
          }
 
-         if (word.length() > 1)
-               child.insert(word.substring(1));
-         else
-               child.isWord = true;
+         if (word.length() > 1) {
+            child.insert(word.substring(1));
+         }
+         else {
+            child.isWord = true;
+         }
       }
 
    }
 
    TrieNode root;
 
+   public Trie() {
+      root = new TrieNode();
+   }
+
    public Trie(List<String> words) {
       root = new TrieNode();
-      for (String word : words)
+      for (String word : words) {
          root.insert(word);
-
+      }
    }
 
    public boolean find(String prefix, boolean exact) {
       TrieNode lastNode = root;
       for (char c : prefix.toCharArray()) {
          lastNode = lastNode.children.get(c);
-         if (lastNode == null)
-               return false;
+         if (lastNode == null) { return false; }
       }
-      return !exact || lastNode.isWord;
+      return (!exact || lastNode.isWord);
    }
 
    public boolean find(String prefix) {
